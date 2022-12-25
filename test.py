c159f12ee1effcd494f58d29c8e05552fe975706
tair3 <tair31@gmail.com>
2022-12-25 14:41:23 +0200

    add model summary option

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import utils
 import argparse
-import pickle
 
 
 def test():
@@ -10,6 +9,7 @@ def test():
     parser.add_argument("--data_path", help="path for the data to load", type=str, default="data")
     parser.add_argument("--verbose", help="verbosity", default=1)
     parser.add_argument("--seed", help="seed for reproducible train/test distribution", default=123)
+    parser.add_argument("--model_summary", help="plot the model summary", default=False)
 
     args = parser.parse_args()
     args = vars(args)
@@ -18,6 +18,9 @@ def test():
     model = tf.keras.models.load_model(
         args["model_path"], custom_objects=None, compile=True, options=None
     )
+
+    if args["model_summary"]:
+        print(model.summary())
 
     x_test, y_test = utils.test_data_load(args["data_path"], False, args["seed"])
 
--- train.py
+++ train.py
@@ -15,12 +15,13 @@ def main():
     parser.add_argument("--output_size", help="output size, number of classes",
                         default=2)
     parser.add_argument("--learning_rate", help="learning rate for network training", default=0.0005)
-    parser.add_argument("--epochs", help="number of epochs for training", default=60)
+    parser.add_argument("--epochs", help="number of epochs for training", type=int, default=60)
     parser.add_argument("--optimizer", help="optimizer for training", default='adam')
     parser.add_argument("--verbose", help="verbosity", default=1)
     parser.add_argument("--flag_visualizations", help="flag for presenting plots of the training process", default=1)
     parser.add_argument("--save_path", help="save model path", default="checkpoints")
     parser.add_argument("--seed", help="seed for reproducible train/test distribution", default=123)
+    parser.add_argument("--model_summary", help="plot the model summary", default=False)
 
     args = parser.parse_args()
     args_dict = vars(args)
@@ -38,6 +39,9 @@ def main():
                                                     lr=args.learning_rate,
                                                     verbose=args.verbose,
                                                     )
+
+    if args.model_summary:
+        print(model.summary())
 
     utils.print_evaluation(model, x_test, y_test, verbose=args.verbose)
     tf.keras.models.save_model(model, args.save_path)
